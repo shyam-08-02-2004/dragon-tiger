c29db048f677e975db1b4b32f4a4943cb1e24241
shyam-08-02-2004/form <shyamdangi084@gmail.com>
2026-06-08 13:54:25 +0530

    fix: route api calls to vercel backend

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+const originalFetch = window.fetch;
+window.fetch = async (...args) => {
+  let [resource, config] = args;
+  if (typeof resource === 'string' && resource.startsWith('/api')) {
+    resource = 'https://dragon-tiger-shyam-babu-s-projects.vercel.app' + resource;
+  } else if (resource instanceof Request && resource.url.startsWith('/api')) {
+    resource = new Request('https://dragon-tiger-shyam-babu-s-projects.vercel.app' + resource.url, resource);
+  }
+  return originalFetch(resource, config);
+};
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
